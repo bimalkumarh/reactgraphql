a967c41604ac274adde4f1381e59f48c0409a4c5
Bimal Kumar H <bimalkumarh@gmail.com>
2025-03-31 18:29:07 +0530

    token maskinh

---
+++ src/db.js
@@ -0,0 +1,10 @@
+const github = {
+    baseURL: "https://api.github.com/graphql",
+    username: "bimalkumarh",
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer <GIT TOKEN HERE>`,
+    },
+}
+
+export default github;
